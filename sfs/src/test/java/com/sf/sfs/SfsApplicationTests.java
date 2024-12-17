c719b0d2a53f9b349277d74d89b1715fff79f3d1
Sanghyeok-gwak <sb02380@naver.com>
2024-12-17 11:37:56 +0900

    Initial commit

---
+++ sfs/src/test/java/com/sf/sfs/SfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.sfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
